32b2534e8de6869df0615b16e91ac9336927205c
Priyanka Miranda <priyankamiranda2@gmail.com>
2019-11-03 19:26:05 +0530

    Filtering and sorting version 1

--- main.py
+++ main.py
@@ -4,13 +4,32 @@ import os.path
 import utils  
 
 def main():
+
 	obj=utils.operations()
+
 	obj.csv_to_json()
-	obj.load_json()
-	obj.search(input())
 
+	# initialize with all data
+	data=obj.load_json()
+	choices=obj.get_titles(data)
 
+	data = obj.search(input(),data	)
+	print(data)
+	print(choices)
 
+	data=obj.sort(choices[int(input())],data)
+	
+	print(data)
+	print("Filtering")
+	print(choices)
+
+	level1_choice=choices[int(input())]
+	choices2=obj.get_options(level1_choice,data)
+	print(choices2)
+	level2_choice=choices2[int(input())]
+	data=obj.filter(level1_choice,level2_choice,data)
+
+	print(data)
 
 if __name__=="__main__":
 	main()
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ class operations:
 		self.json_data=None
 
 	def load_json(self):
-		self.json_data = json.load(open(constants.data_loc+constants.file+'_json.json'))
+		return json.load(open(constants.data_loc+constants.file+'_json.json'))
 
 	# Converting to json
 	def csv_to_json(self):
@@ -15,29 +15,53 @@ class operations:
 		df=self.remove_cols(df)
 		df.to_json(constants.data_loc+constants.file+'_json.json', orient='records')
 
-	def get_titles(self):
-		self.load_json()
-		df = pd.DataFrame.from_dict(self.json_data, orient='columns')
-		return df.columns.values
+	def get_titles(self,data):
+		try:
+			df = pd.DataFrame.from_dict(data, orient='columns')
+			df = self.remove_spaces(df)
+			return df.columns.values
+		except:
+			return []
 
-	def remove_spaces(self,df):
+	def get_options(self,choice,data):
+		try:
+			df = pd.DataFrame.from_dict(data, orient='columns')
+			df = self.remove_spaces(df)
+			return df[choice].unique()
+		except:
+			return []
+
+	def remove_spaces(self, df):
 		redo_header=[x.strip(' ') for x in list(df.columns.values)]
 		df.columns = redo_header
 		return df
 
-	def remove_cols(self,df):
+	def remove_cols(self, df):
 		df = df.iloc[1:]# removing second column
 		df = df.iloc[:-1]# removing last column
-		df=self.remove_spaces(df)
+		df = self.remove_spaces(df)
 		df.drop(constants.remove_cols, axis=1, inplace=True)
 		return df
 
-	def search(self,text):	
-		self.load_json() 
-		df = pd.DataFrame.from_dict(self.json_data, orient='columns')
-		df=self.remove_spaces(df)
+	def search(self, text, data):	
+		df = pd.DataFrame.from_dict(data, orient='columns')
+		df = self.remove_spaces(df)
 		df3 = pd.DataFrame(columns=df.columns.values)
-		for elems in constants.search_cols:
-			df2=df[(df[elems] == text)]
+		for elem in constants.search_cols:
+			df2=df[(df[elem] == text)]
 			df3=pd.concat([df3,df2], axis=0)
 		return df3.to_dict('records')
+
+	def sort(self, title, data):
+		try:
+			df = pd.DataFrame.from_dict(data, orient='columns')
+			df = self.remove_spaces(df)
+			return df.sort_values(by=[title], ascending=True).to_dict('records')
+		except:
+			return []
+
+	def filter(self,elem, inside_choice,data):
+		df = pd.DataFrame.from_dict(data, orient='columns')
+		df = self.remove_spaces(df)
+		df=df[(df[elem] == inside_choice)]
+		return df.to_dict('records')
