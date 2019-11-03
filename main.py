d9f5ffa41346c392187ad5a316ecbda1fb10338d
Priyanka Miranda <priyankamiranda2@gmail.com>
2019-11-03 20:33:08 +0530

    flask setup

--- main.py
+++ main.py
@@ -1,35 +1,69 @@
-import csv
+
 import json
-import os.path
 import utils  
 
-def main():
+from flask import Flask
+import requests
 
+app = Flask(__name__)
+
+@app.route("/")
+def start():
 	obj=utils.operations()
-
 	obj.csv_to_json()
-
-	# initialize with all data
 	data=obj.load_json()
-	choices=obj.get_titles(data)
+	return data
 
-	data = obj.search(input(),data	)
-	print(data)
-	print(choices)
+# choices=obj.get_titles(data)
 
-	data=obj.sort(choices[int(input())],data)
-	
-	print(data)
-	print("Filtering")
-	print(choices)
+# data = obj.search(input(),data	)
+# print(data)
+# print(choices)
 
-	level1_choice=choices[int(input())]
-	choices2=obj.get_options(level1_choice,data)
-	print(choices2)
-	level2_choice=choices2[int(input())]
-	data=obj.filter(level1_choice,level2_choice,data)
+# data=obj.sort(choices[int(input())],data)
 
-	print(data)
+# print(data)
+# print("Filtering")
+# print(choices)
+
+# level1_choice=choices[int(input())]
+# choices2=obj.get_options(level1_choice,data)
+# print(choices2)
+# level2_choice=choices2[int(input())]
+# data=obj.filter(level1_choice,level2_choice,data)
+
+# print(data)
 
 if __name__=="__main__":
-	main()
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ class operations:
 	def csv_to_json(self):
 		df = pd.DataFrame(pd.read_csv(constants.data_loc+constants.file+".csv", sep = constants.csv_file_separator, header = 0, index_col = False))
 		df=self.remove_cols(df)
-		df.to_json(constants.data_loc+constants.file+'_json.json', orient='records')
+		df.to_json(constants.data_loc+constants.file+'_json.json', orient='columns')
 
 	def get_titles(self,data):
 		try:
@@ -50,13 +50,13 @@ class operations:
 		for elem in constants.search_cols:
 			df2=df[(df[elem] == text)]
 			df3=pd.concat([df3,df2], axis=0)
-		return df3.to_dict('records')
+		return df3.to_dict('columns')
 
 	def sort(self, title, data):
 		try:
 			df = pd.DataFrame.from_dict(data, orient='columns')
 			df = self.remove_spaces(df)
-			return df.sort_values(by=[title], ascending=True).to_dict('records')
+			return df.sort_values(by=[title], ascending=True).to_dict('columns')
 		except:
 			return []
 
@@ -64,4 +64,4 @@ class operations:
 		df = pd.DataFrame.from_dict(data, orient='columns')
 		df = self.remove_spaces(df)
 		df=df[(df[elem] == inside_choice)]
-		return df.to_dict('records')
+		return df.to_dict('columns')
